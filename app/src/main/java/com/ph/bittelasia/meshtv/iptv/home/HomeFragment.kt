2bb23a4837b62d2cd1a9c2daf35a5d9f8409859c
mars <mraraullo@gmail.com>
2021-09-14 13:47:48 +0800

    Messaging

--- app/src/main/java/com/ph/bittelasia/meshtv/iptv/home/HomeFragment.kt
+++ app/src/main/java/com/ph/bittelasia/meshtv/iptv/home/HomeFragment.kt
@@ -1,7 +1,6 @@
 package com.ph.bittelasia.meshtv.iptv.home
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
